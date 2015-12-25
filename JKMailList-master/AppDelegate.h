37b62a2bad8e4570567cb0bc016f5a2b231329bb
wangruifeng <wangruifeng@xiangrikui.com>
2015-12-25 14:12:34 +0800

    Initial Commit

---
+++ JKMailList-master/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKMailList-master
+//
+//  Created by 王锐锋 on 15/12/25.
+//  Copyright © 2015年 jack_wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKMailList-master/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKMailList-master
+//
+//  Created by 王锐锋 on 15/12/25.
+//  Copyright © 2015年 jack_wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
